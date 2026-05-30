41ce775baa5b1d9254c4bc66384864416d232508
RicardoLeoTer <123416618+RicardoLeoTer@users.noreply.github.com>
2026-05-29 23:09:05 -0600

    Add files via upload

---
+++ index.css
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Catalog from './pages/Catalog';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/catalogo" element={<Catalog />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
